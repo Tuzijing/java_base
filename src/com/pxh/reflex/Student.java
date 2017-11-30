97dfe1f340ad8784c9b9a5f7730fdfe1fa529aaf
Tuzijing <2929448430@qq.com>
2017-11-30 15:05:34 +0800

    add Student_class

--- src/com/pxh/reflex/Student.java
+++ src/com/pxh/reflex/Student.java
@@ -22,5 +22,70 @@ package com.pxh.reflex;
  *
  */
 public class Student {
+	private int id; 
+	private String name;
+	/**
+	 *
+	 * @Time:2017年11月30日 下午2:52:49
+	 * @Username:Peng xh
+	 * @Todo: Non parametric construction
+	 */
+	public Student() {}
 
+	
+
+	/**
+	 *
+	 * @Time:2017年11月30日 下午3:01:47
+	 * @Username:Peng xh
+	 * @Todo:One parameters construct
+	 * @param id
+	 */
+	private Student(int id) {
+		super();
+		this.id = id;
+	}
+
+
+	/**
+	 *
+	 * @Time:2017年11月30日 下午2:52:23
+	 * @Username:Peng xh
+	 * @Todo:Tow parameters construction
+	 * @param id 
+	 * @param name
+	 */
+	public Student(int id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	/**
+	 * 
+	  * @Time:2017年11月30日 下午3:04:45
+	  * @Author:Peng Xh
+	  * @Description: test1
+	  * @git 
+	  * @Return void
+	  * History:
+	  * <author>          <time>          <version>          <desc>
+	  * 作者姓名           修改时间           版本号              描述
+	 */
+	public void test1(){
+		System.out.println("");
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
